1be75142389d8d3e08daf466900b158e8e7aa39e
wejdev <wejdev@gmail.com>
2022-02-04 23:11:04 -0800

    Consolidate test and tests folders

--- tests/wordlehintfinder.test.ts
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-import { computeRegexsForHints, knownHints } from '../src/wordlehintfinder';
-
-test('Wordle map returns correct regular expressions', () => {
-
-    expect(true).toBeTruthy;
-});
