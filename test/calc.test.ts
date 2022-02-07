81e208c46a54a23b0a40cc95f7bc7e1538fc0755
wejdev <wejdev@gmail.com>
2022-02-06 22:14:22 -0800

    Consolidate add() tests

--- test/calc.test.ts
+++ test/calc.test.ts
@@ -1,23 +1,13 @@
 import { add, mul } from "../src/calc";
 
 describe("test add function", () => {
-    it("should return 15 for add(10,5)", () => {
-        expect(add(10, 5)).toBe(15);
-    });
-
-    it("should return 5 for add(2,3)", () => {
-        expect(add(2, 3)).toBe(5);
-    });
-
-    it("should return 5 for add(2,3)", () => {
-        expect(add(2, 3)).toBe(5);
-    });
-
     test.each(
         [
             [1, 1, 2],
             [-1, 2, 1],
-            [2, 1, 3]
+            [2, 1, 3],
+            [2, 3, 5],
+            [10, 5, 15]
         ])(
             'add( %i, %i) should return %i', (a, b, expected) => {
                 expect(add(a, b)).toBe(expected);
