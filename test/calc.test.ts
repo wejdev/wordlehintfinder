88eeeebef2f6f14a97e7947f33ec5b2a9a65fc4e
wejdev <wejdev@gmail.com>
2022-02-06 21:57:47 -0800

    Added parameterized test for add()

--- test/calc.test.ts
+++ test/calc.test.ts
@@ -12,7 +12,19 @@ describe("test add function", () => {
     it("should return 5 for add(2,3)", () => {
         expect(add(2, 3)).toBe(5);
     });
+
+    test.each(
+        [
+            [1, 1, 2],
+            [-1, 2, 1],
+            [2, 1, 3]
+        ])(
+            'add( %i, %i) should return %i', (a, b, expected) => {
+                expect(add(a, b)).toBe(expected);
+            },
+        );
 });
+
 
 describe("test mul function", () => {
     it("should return 15 for mul(3,5)", () => {
